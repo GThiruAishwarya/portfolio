46c0966757609b3d1a664b2efce636b1591e90f0
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-21 12:53:42 +0530

    Home.jsx

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const Home=()=>{
+return(
+  <section>
+  <h1>Hi, I'm Gotte Thiru Aishwarya Yadav</h1>
+  <p>I am a Full Stack Developer</p>
+  <a href="/projects">
+  <button>View Projects</button>
+  </a>
+  </section>
+  )
+}
+export default Home
